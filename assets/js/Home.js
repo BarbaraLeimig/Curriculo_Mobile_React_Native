2ea1726c68d7c8b38c2aadd80987cf564943499e
BarbaraLeimig <barbaraleimigdev@gmail.com>
2024-05-20 07:51:27 -0300

    correção de identação

--- assets/js/Home.js
+++ assets/js/Home.js
@@ -1,31 +1,39 @@
 // SobreRestaurante.js
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity, Linking } from "react-native";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Entypo } from '@expo/vector-icons';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Entypo } from "@expo/vector-icons";
 
 const Home = () => {
-
-  /* fazer aqui um const linkedin e cons github, depois faze o esquema que nem o navigate do App.js */
-  
-    const linkedin = () => {
-      Linking.openURL('https://www.linkedin.com/in/barbaraleimig/');
-    };
-    const github = () => {
-      Linking.openURL('https://github.com/BarbaraLeimig');
-    };
-  
+  const linkedin = () => {
+    Linking.openURL("https://www.linkedin.com/in/barbaraleimig/");
+  };
+  const github = () => {
+    Linking.openURL("https://github.com/BarbaraLeimig");
+  };
 
   return (
-    <View style={styles.container}> 
-      <Image source={require('../imgs/barbara.jpg')} style={styles.homeImage} />
+    <View style={styles.container}>
+      <Image source={require("../imgs/barbara.jpg")} style={styles.homeImage} />
       <Text style={styles.title}>Bárbara Leimig</Text>
       <Text style={styles.info}>Associate Full Stack Developer</Text>
       <View style={styles.iconContainer}>
         <TouchableOpacity onPress={linkedin}>
-          <FontAwesome style={styles.icon} name="linkedin" size={37} color="black" />
+          <FontAwesome
+            style={styles.icon}
+            name="linkedin"
+            size={37}
+            color="black"
+          />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.icon} onPress={github}> 
+        <TouchableOpacity style={styles.icon} onPress={github}>
           <Entypo name="github" size={37} color="black" />
         </TouchableOpacity>
       </View>
@@ -50,20 +58,20 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   homeImage: {
-    width: 200, 
-    height: 200, 
-    borderRadius: 100, 
+    width: 200,
+    height: 200,
+    borderRadius: 100,
     resizeMode: "cover",
   },
   iconContainer: {
     marginTop: 15,
     flexDirection: "row",
-    alignItens: "baseline", 
+    alignItens: "baseline",
   },
   icon: {
     marginRight: 25,
-    marginLeft: 25, 
-  }
+    marginLeft: 25,
+  },
 });
 
 export default Home;
